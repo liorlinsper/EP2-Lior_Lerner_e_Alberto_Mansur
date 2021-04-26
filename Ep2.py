ba3b32c87d8ae9e53bc07a194adfee96750d15b7
betomansur <81189941+betomansur@users.noreply.github.com>
2021-04-26 12:31:06 -0300

    Update Ep2.py

--- Ep2.py
+++ Ep2.py
@@ -2,3 +2,13 @@
 def cria_baralho():
     lista_de_cartas = ['2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠','A♠','2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥','A♥','2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣','A♣','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦','A♦']
     return lista_de_cartas
+
+def extrai_naipe(carta):
+    if "♠" in carta:
+        return "♠"
+    elif "♥" in carta:
+        return "♥"
+    elif "♣" in carta:
+        return "♣"
+    elif "♦" in carta:
+        return "♦"
