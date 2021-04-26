e5a2de3a7c7692bd5f4deaeabe86a6e1adc72228
betomansur <81189941+betomansur@users.noreply.github.com>
2021-04-26 12:35:15 -0300

    Update Ep2.py

--- Ep2.py
+++ Ep2.py
@@ -13,3 +13,32 @@ def extrai_naipe(carta):
         return "♣"
     elif "♦" in carta:
         return "♦"
+    
+#Extração de valores:
+def extrai_valor(carta):
+    if "A" in carta:
+        return "A"
+    elif "2" in carta:
+        return "2"
+    elif "3" in carta:
+        return "3"
+    elif "4" in carta:
+        return "4"
+    elif "5" in carta:
+        return "5"
+    elif "6" in carta:
+        return "6"
+    elif "7" in carta:
+        return "7"
+    elif "8" in carta:
+        return "8"
+    elif "9" in carta:
+        return "9"
+    elif "10" in carta:
+        return "10"
+    elif "J" in carta:
+        return "J"
+    elif "Q" in carta:
+        return "Q"
+    elif "K" in carta:
+        return "K"
