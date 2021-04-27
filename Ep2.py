a5e66fec93077ddbd2f046d38573e5cf775f7abb
liorlinsper <81189706+liorlinsper@users.noreply.github.com>
2021-04-26 23:15:29 -0300

    Update Ep2.py

--- Ep2.py
+++ Ep2.py
@@ -46,31 +46,34 @@ def extrai_valor(carta):
 # Movimentos possíveis para as cartas:
     
 def lista_movimentos_possiveis(baralho,posicao):
+
     movimentos_gerais = [0]*len(baralho)
     #Caso o índice seja = 0
     movimentos_gerais[0] = []
+
     #Outros índices:
-    i = 1
+    i = 1 
     while i < len(baralho):
         #Para posições < 3:
         if i < 3:
-            if (baralho[i-1][0] == baralho[i][0]) or (baralho[i-1][1] == baralho[i][1]):
+            if (baralho[i-1][0] == baralho[i][0]) or (baralho[i-1][1] == baralho[i][-1]):
                 movimentos_gerais[i] = [1]
             else:
                 movimentos_gerais[i] = []
         #Para posições > 3:
         if i >=3:    
             #Caso a carta possa realizar qualquer movimento: 
-            if  ((baralho[i-1][0] == baralho[i][0]) and (baralho[i-3][0] == baralho[i][0])) or ((baralho[i-1][1] == baralho[i][1]) and (baralho[i-3][1] == baralho[i][1])) or ((baralho[i-1][0] == baralho[i][0]) and (baralho[i-3][1] == baralho[i][1])) or ((baralho[i-1][1] == baralho[i][1]) and (baralho[i-3][0] == baralho[i][0])):
+            if  ((baralho[i-1][0] == baralho[i][0]) and (baralho[i-3][0] == baralho[i][0])) or ((baralho[i-1][-1] == baralho[i][-1]) and (baralho[i-3][-1] == baralho[i][-1])) or ((baralho[i-1][0] == baralho[i][0]) and (baralho[i-3][-1] == baralho[i][-1])) or ((baralho[i-1][-1] == baralho[i][-1]) and (baralho[i-3][0] == baralho[i][0])):
                 movimentos_gerais[i] = [1,3]
             #Caso a carta possa  ser apenas empilhada:
-            elif (baralho[i-1][0] == baralho[i][0]) or (baralho[i-1][1] == baralho[i][1]):
+            elif (baralho[i-1][0] == baralho[i][0]) or (baralho[i-1][-1] == baralho[i][-1]):
                 movimentos_gerais[i] = [1]
             #Caso a carta possa ser emplihada apenas sobre o terceiro vizinho anterior:
-            elif (baralho[i-3][0] == baralho[i][0]) or (baralho[i-3][1] == baralho[i][1]):
+            elif (baralho[i-3][0] == baralho[i][0]) or (baralho[i-3][-1] == baralho[i][-1]):
                 movimentos_gerais[i] = [3]
             #Caso a carta não tenha movimentos possíveis:
             else:
                 movimentos_gerais[i] = []
         i += 1
-    return (movimentos_gerais)
+    return (movimentos_gerais[posicao])
+
