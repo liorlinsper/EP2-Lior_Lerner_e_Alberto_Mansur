566121f6ef0d2d45bf24572e903dbe25fbfefd7e
betomansur <81189941+betomansur@users.noreply.github.com>
2021-04-27 15:17:23 -0300

    Update Jogo_EP2.py

--- Jogo_EP2.py
+++ Jogo_EP2.py
@@ -10,10 +10,34 @@ while i < len(baralho):
     i+=1
 
 #JOGO:
- 
-pode_jogar = Funcoes_EP2.possui_movimentos_possiveis(baralho)
 
+i = 0
+while i < len(baralho):
+    print("{0}. {1}".format(i+1,baralho[i]))
+    i+=1
+
+baralho1 = baralho
+pode_jogar = Funcoes_EP2.possui_movimentos_possiveis(baralho)
 while pode_jogar == True:
+    i = 0
+    while i < len(baralho):
+        print("{0}. {1}".format(i+1,baralho[i]))
+        i+=1
     esc_carta = int(input("Escolha uma carta( digite um numero entre 1 e 52):"))
+    movimentos_possiveis = Funcoes_EP2.lista_movimentos_possiveis(baralho,esc_carta)
     if esc_carta < 0 or esc_carta > 52:
         print("Movimento Inválido")
+    elif movimentos_possiveis == []:
+        print("Não há movimentos possíveis")
+    elif movimentos_possiveis == [1,3]:
+        opcao  = int(input("Qual opção(1 ou 3): "))
+        if opcao == 1:
+            baralho1 = Funcoes_EP2.empilha(baralho,esc_carta-1,esc_carta-2)
+        elif opcao == 3:
+            baralho1 = Funcoes_EP2.empilha(baralho,esc_carta-1,esc_carta-4)
+    elif movimentos_possiveis == [1]:
+        baralho1 = Funcoes_EP2.empilha(baralho,esc_carta-1,esc_carta-2)
+    elif movimentos_possiveis == [3]:
+        baralho1 = Funcoes_EP2.empilha(baralho,esc_carta-1,esc_carta-4)
+
+    print(baralho1)
