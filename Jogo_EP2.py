64973ec8758ae5c99828610860b8af7a4dc2a7a0
liorlinsper <81189706+liorlinsper@users.noreply.github.com>
2021-04-27 14:52:18 -0300

    Update Jogo_EP2.py

--- Jogo_EP2.py
+++ Jogo_EP2.py
@@ -9,4 +9,11 @@ while i < len(baralho):
     print("{0}. {1}".format(i+1,baralho[i]))
     i+=1
 
-esc_carta = int(input("Escolha uma carta( digite um numero entre 1 e 52):"))
+#JOGO:
+ 
+pode_jogar = Funcoes_EP2.possui_movimentos_possiveis(baralho)
+
+while pode_jogar == True:
+    esc_carta = int(input("Escolha uma carta( digite um numero entre 1 e 52):"))
+    if esc_carta < 0 or esc_carta > 52:
+        print("Movimento Inválido")
