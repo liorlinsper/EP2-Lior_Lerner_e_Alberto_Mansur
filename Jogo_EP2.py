29cdd5503cad66d2224d326629aa96d0a5daeea4
liorlinsper <81189706+liorlinsper@users.noreply.github.com>
2021-04-27 14:39:33 -0300

    Update Jogo_EP2.py

--- Jogo_EP2.py
+++ Jogo_EP2.py
@@ -6,7 +6,7 @@ random.shuffle(baralho)
 #Colocando o baralho em ordem
 i = 0
 while i < len(baralho):
-    print("{0}. {1}".format(i+1,baralho[i]))
+    print("Mesa: {0}. {1}".format(i+1,baralho[i]))
     i+=1
 
 esc_carta = int(input("Escolha uma carta( digite um numero entre 1 e 52):"))
