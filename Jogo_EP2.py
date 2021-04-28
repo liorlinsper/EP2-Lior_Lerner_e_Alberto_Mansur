5839aecd3a09d660fee91f939386c95c8c6e260d
betomansur <81189941+betomansur@users.noreply.github.com>
2021-04-28 14:55:28 -0300

    Update Jogo_EP2.py

--- Jogo_EP2.py
+++ Jogo_EP2.py
@@ -1,3 +1,20 @@
+print("Paciência Acordeão ")
+print("================== ")
+print("")
+print("Seja bem-vindo(a) ao jogo de Paciência Acordeão! O objetivo deste jogo é colocar todas as cartas em uma mesma pilha. ")
+print("")
+print("Existem apenas dois movimentos possíveis: ")
+print("")
+print("1. Empilhar uma carta sobre a carta imediatamente anterior; ")
+print("2. Empilhar uma carta sobre a terceira carta anterior. ")
+print("")
+print("Para que um movimento possa ser realizado basta que uma das duas condições abaixo seja atendida: ")
+print("")
+print("1. As duas cartas possuem o mesmo valor ou ")
+print("2. As duas cartas possuem o mesmo naipe. ")
+print("")
+print("Desde que alguma das condições acima seja satisfeita, qualquer carta pode ser movimentada. ")
+
 import Funcoes_EP2
 import random
 # Misturando baralho
